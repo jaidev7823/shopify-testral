db388bb4c7b90bd628dbd4255621c247be5867a2
jaidev7823 <jaidev7823@gmail.com>
2026-03-23 01:47:11 +0530

    changed env url path

--- app/hooks/useEnv.ts
+++ app/hooks/useEnv.ts
@@ -4,7 +4,7 @@ import { useRouteLoaderData } from "react-router";
 export function useEnv() {
   return useRouteLoaderData("root") as {
     ENV: {
-      PUBLIC_BASE_URL: string;
+      PUBLIC_BASE_URL: process.env.SHOPIFY_APP_URL;
       DATABASE_URL: string;
     };
   };
